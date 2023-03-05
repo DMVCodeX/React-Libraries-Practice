207d2d474c27a25f42b32580823b4b7e7350bbf8
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-05 13:12:21 -0500

    Add third party library youtube player

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Content />
       <Footer />
+      <ReactPlayer url="https://www.youtube.com/watch?v=U0eLmyJkQBc" />
     </div>
   );
 }
